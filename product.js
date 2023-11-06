5cf922131b0137d429b2def6e90e3d1a3ab90a55
Your NameNaveen_2023 <13naveen2003@gmail.com>
2023-11-06 22:34:44 +0530

    little

--- product.js
+++ product.js
@@ -1,24 +1,56 @@
 window.addEventListener('DOMContentLoaded',function(){
-    var catogaries = document.querySelector(".catogaries");
-    var productTemplate = document.querySelector(".product");
+    var nav_link = document.getElementById("nav-link");
+    var logo = document.getElementById("logo");   
+     var productTemplate = document.querySelector(".product");
     var products_container = document.querySelector(".products");
-    catogaries.style.top = "20px";
-    catogaries.style.transition = "0.7s";
+    var products_catogaries = document.querySelector(".product-catogaries");
+
+    nav_link.style.right = "0px";
+    nav_link.style.transition = ".7s"
+    logo.style.left = "0px";
+    logo.style.transition=".7s"
+    products_catogaries.style.left = "0px";
+    products_catogaries.style.transition=".7s"
     
-  products.forEach(product=>{
-    const templateClone = productTemplate.cloneNode(true)
-    document.querySelector('.name').textContent = product.name;
-    document.querySelector('.description').textContent=product.description;
-    document.querySelector('.old-price').textContent=product.old;
-    document.querySelector('.new-price').textContent=product.price;
-    products_container.appendChild(templateClone);
+//   products.forEach(product=>{
+//     const templateClone = productTemplate.cloneNode(true);
+//     document.querySelector('.name').textContent = product.name;
+//     document.querySelector('.description').textContent=product.description;
+//     document.querySelector('.old_price').textContent=product.old;
+//     document.querySelector('.new_price').textContent=product.price;
+//     products_container.appendChild(templateClone);
     
-  })
+//   })
 })
+
+// Function to populate the product list
+
+    // products.forEach(function(product) {
+    //     const product_list = document.createElement("div");
+    //     product_list.classList.add('product');
+    //     product_list.innerHTML = `
+        
+    //     <div class="img">
+    //         <img src="./images/dell_01.jpg" alt="">
+    //     </div>
+    //     <h2 class="name">${product.name}</h2>
+    //     <p class="description">${product.description}</p>
+    //        <div class="prices">
+    //          <span class="old_price">${product.old}</span><h3 class="new_price">${product.new}</h3>
+    //        </div>
+    //        <span class="discount"></span>
+    //        <button class="btn-fav">
+    //         <i class="fa fa-heart"></i>
+    //        </button>
+    
+    //     `;
+    //     products_container.appendChild(product_lis);
+    // });
+
 
 const products = [
     {
-      name: "Mouse",
+      name: "Mouses",
       old:29.99,
       price: 19.99,
       description: "This is the first product.",
